056369f923c87395158fbf14d716456fa3bd8dd7
HISEHOONAN <dkstpgns0617@gmail.com>
2025-03-12 15:12:21 +0900

    [INIT] Add DesignSystem and etc

---
+++ Pinit/Pinit/Views/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Pinit
+//
+//  Created by nelime on 3/10/25.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+    
+    private let Label : UILabel = {
+        let label = UILabel()
+        label.text = "한글 123 #%^$"
+        label.font = DesignSystemFont.Pretendard_Bold70.value
+        return label
+    }()
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        view.addSubview(Label)
+        
+        Label.snp.makeConstraints{
+            $0.center.equalToSuperview()
+        }
+    }
+    
+    
+    
+    
+}
+
+
+#Preview{
+    ViewController()
+}
