e6db7478cd354a3470c345db75ee45fc2baa041a
HISEHOONAN <dkstpgns0617@gmail.com>
2025-03-13 22:51:14 +0900

    [FEAT] #5 Add FSCalendar

--- Pinit/Pinit/Views/PinEdit/PinEditViewController.swift
+++ Pinit/Pinit/Views/PinEdit/PinEditViewController.swift
@@ -7,11 +7,33 @@
 
 
 import UIKit
+import FSCalendar
+import SnapKit
 
 final class PinEditViewController: UIViewController {
+    
+    private let PinCalendar : FSCalendar = {
+        let calendar = FSCalendar()
+        calendar.appearance.selectionColor = .systemBlue
+        return calendar
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        SeUI()
     }
+    
+    private func SeUI() {
+        view.addSubviews(PinCalendar)
+        
+        PinCalendar.snp.makeConstraints{
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(370)
+        }
+    }
+    
+    
 }
 
 #Preview {
