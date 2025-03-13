87fe02c289eddcfb32173d1510a67ab9efdb6392
JungHm <taggma12@gmail.com>
2025-03-13 13:35:47 +0900

    [FEAT] #5 Custom BottomSheet의 CollectionView를 Adapter와 연결 및 BottomSheet Custom Gesture 추가

--- Pinit/Pinit/Views/Home/HomeViewController.swift
+++ Pinit/Pinit/Views/Home/HomeViewController.swift
@@ -10,14 +10,17 @@ import MapKit
 import SnapKit
 
 class HomeViewController: UIViewController {
-//    private let bottomSheet: 
+    
+    private var adapter: PinCollectionViewAdapter?
+    
+    private let bottomSheet = CustomBottomSheet()
     private let Label : UILabel = {
         let label = UILabel()
         label.text = "한글 123 #%^#$"
         label.font = DesignSystemFont.Pretendard_Bold70.value
         return label
     }()
-    
+    private var bottomSheetHeightConstraint: Constraint!
 
     
     override func viewDidLoad() {
@@ -25,14 +28,51 @@ class HomeViewController: UIViewController {
         view.backgroundColor = .white
         view.addSubview(Label)
         
+        adapter = PinCollectionViewAdapter(
+            collectionView: bottomSheet.collectionView,
+            width: view.frame.width
+        )
+        adapter?.delegate = self
+        
         Label.snp.makeConstraints{
             $0.center.equalToSuperview()
         }
     }
     
+    func setupBottomSheet() {
+        // 높이 조절을 위한 제약 설정
+        bottomSheet.snp.makeConstraints {
+            let initialHeight = view.frame.width / 6
+            bottomSheetHeightConstraint = $0.height.equalTo(initialHeight).constraint
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+        // 높이 변경
+        bottomSheetHeightConstraint.update(offset: 500)
+        
+        // 패닝 제스처
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(panGestureHandler))
+//        bottomSheet.grabber.addGestureRecognizer(panGesture)
+        bottomSheet.addGestureRecognizer(panGesture)
+    }
+}
+
+extension HomeViewController {
+    @objc private func panGestureHandler() {
+        
+    }
+}
+
+extension HomeViewController: PinCollectionViewAdapterDelegate {
+    func selectedItem(selected: PinEntity) {
+        print("Selected: \(selected)")
+        // 여기서 화면 이동
+    }
     
-    
-    
+    func deletedItem(deleted: PinEntity?) {
+        print("Deleted: \(deleted)")
+        // 여기서 CoreData 업데이트
+    }
 }
 
 
