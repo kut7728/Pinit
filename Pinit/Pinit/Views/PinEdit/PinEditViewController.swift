564f3796fad04e4cb963533fa73773bbe7add4f2
lkhwan0204 <lkhwan0204@gmail.com>
2025-03-14 15:10:39 +0900

    [FEAT] #8 카메라 버튼 밑에 제목 작성 텍스트필드 추가

--- Pinit/Pinit/Views/PinEdit/PinEditViewController.swift
+++ Pinit/Pinit/Views/PinEdit/PinEditViewController.swift
@@ -3,7 +3,7 @@
 //  Pinit
 //
 //  Created by 안정흠이형메롱메롱메롱메롱바보바보바바바바보바보바보 on 3/12/25.
-//
+// 푸시할때 조심 씬델리게이트 바꿔라
 
 
 import UIKit
@@ -16,6 +16,8 @@ final class PinEditViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = .white      //시뮬레이터 돌려보니 검은색 나와서 흰색으로 했ㄷ
         
         mapView = MKMapView(frame: self.view.bounds)    //mkmapview 초기화 및 뷰 추가함
         mapView = MKMapView(frame: CGRect(x: 0, y: 60, width: self.view.bounds.width, height: self.view.bounds.height / 4)) //화면의 1/4만 나오게 함
@@ -34,9 +36,9 @@ final class PinEditViewController: UIViewController {
         
         //오토레이아웃 설정
         leftbutton.snp.makeConstraints{
-            $0.leading.equalToSuperview().offset(20) //왼쪽에서 20 떨어짐
+            $0.leading.equalToSuperview().offset(10) //왼쪽에서 10 떨어짐
             $0.top.equalToSuperview().offset(300)   //탑에서 300
-            $0.width.equalTo(200)       //너비 200
+            $0.width.equalTo(190)       //너비 200
             $0.height.equalTo(30)         //높이 30
         }
         
@@ -48,9 +50,9 @@ final class PinEditViewController: UIViewController {
         
         //오토레이아웃 설정
         rightbutton.snp.makeConstraints{
-            $0.leading.equalToSuperview().offset(240)
+            $0.leading.equalToSuperview().offset(230)
             $0.top.equalToSuperview().offset(300)
-            $0.width.equalTo(150)
+            $0.width.equalTo(160)
             $0.height.equalTo(30)
         }
         
@@ -59,13 +61,13 @@ final class PinEditViewController: UIViewController {
         camerabutton.backgroundColor = .systemPink
         if let cameraImage = UIImage(systemName: "camera"){     //크기 조절 지피티
             let largeConfig = UIImage.SymbolConfiguration(pointSize: 80, weight: .regular, scale: .default)
-                let largeImage = cameraImage.withConfiguration(largeConfig)
-                camerabutton.setImage(largeImage, for: .normal)
+            let largeImage = cameraImage.withConfiguration(largeConfig)
+            camerabutton.setImage(largeImage, for: .normal)
         }
         camerabutton.tintColor = .white
         camerabutton.imageView?.contentMode = .scaleAspectFit
         self.view.addSubview(camerabutton)
-
+        
         // Auto Layout 설정
         camerabutton.snp.makeConstraints{
             $0.centerX.equalToSuperview()
@@ -74,9 +76,44 @@ final class PinEditViewController: UIViewController {
             $0.height.equalTo(150)
             
         }
+        
+        // 제목 작성 버튼
+        let titlebutton = UITextField()
+        titlebutton.backgroundColor = .systemPink
+        titlebutton.textColor = .white
+        titlebutton.attributedPlaceholder = NSAttributedString(
+            string: "제목 작성",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.white] //제목작성 글자 흰색으로
+        )
+        titlebutton.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: 0))  // 이거 개쩜 제목 작성 맨 앞에 여백을 주는거임
+        titlebutton.leftViewMode = .always      //항상
+        
+        self.view.addSubview(titlebutton)
+        
+        titlebutton.snp.makeConstraints{
+            $0.leading.equalToSuperview().offset(20)
+            $0.top.equalToSuperview().offset(540)
+            $0.width.equalTo(360)
+            $0.height.equalTo(40)
+        }
+        
+        let keyboardToolbar = UIToolbar()
+        let flexBarButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneBarButton = UIBarButtonItem(title: "완료 ", style: .plain, target: self, action: #selector(doneBtnClicked))
+        keyboardToolbar.items = [flexBarButton, doneBarButton]
+        keyboardToolbar.sizeToFit()
+        keyboardToolbar.tintColor = UIColor.white
+        
+        titlebutton.inputAccessoryView = keyboardToolbar
+        
+        
+    }
+    
+    @objc func doneBtnClicked() {
+        view.endEditing(true)  // 키보드 닫기
     }
 }
 
-#Preview {
+#Preview{
     PinEditViewController()
 }
