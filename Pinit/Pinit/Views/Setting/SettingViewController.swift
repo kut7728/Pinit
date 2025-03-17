f8f5fea777a39cd56c5bead8c66a806ba24a06f1
InTak Han <hits0304@gmail.com>
2025-03-17 13:17:47 +0900

    "[CHORE] 주석 추가"

--- Pinit/Pinit/Views/Setting/SettingViewController.swift
+++ Pinit/Pinit/Views/Setting/SettingViewController.swift
@@ -15,21 +15,18 @@ final class SettingViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         
+        //버튼 레이아웃 설정
         resetButton.setTitle("전체 기록 삭제", for: .normal)
         resetButton.addTarget(self, action: #selector(resetAlert), for: .touchUpInside)
         resetButton.backgroundColor = .lightGray
         
         view.addSubview(resetButton)
         
-        resetButton.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.bottom.equalToSuperview().offset(-100)
-            make.height.equalTo(50)
-            make.leading.equalTo(20)
-        }
+        autoLayout()
     }
     
     @objc func resetAlert() {
+        //버튼이 눌리면 실행되는 부분(Alert)
         let alert = UIAlertController(title: "전체 기록 삭제", message: "모든 핀이 삭제됩니다.\n 진행하시겠습니까?", preferredStyle: .alert)
 
         let addAction = UIAlertAction(title: "확인", style: .destructive) { _ in
@@ -44,6 +41,20 @@ final class SettingViewController: UIViewController {
         present(alert, animated: true)
     }
 }
+//오토레이아웃 제약 설정(snapkit) 부분
+extension SettingViewController {
+    private func autoLayout() {
+        resetButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalToSuperview().offset(-100)
+            $0.height.equalTo(50)
+            $0.leading.equalTo(20)
+        }
+        //컬렉션 뷰 제약 설정 부분 예정
+        
+    }
+}
+
 #Preview {
     SettingViewController()
 }
