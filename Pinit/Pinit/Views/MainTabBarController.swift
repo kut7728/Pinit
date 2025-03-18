1e943a00552d44a32441e8bbe8d458d854b11fce
kut7728 <kut7728@hanyang.ac.kr>
2025-03-18 17:01:52 +0900

    [FEAT] #9 250318 탭바 정상화

--- Pinit/Pinit/Views/MainTabBarController.swift
+++ Pinit/Pinit/Views/MainTabBarController.swift
@@ -45,8 +45,8 @@ final class MainTabBarController: UITabBarController {
     }
     
     private func setupViewControllers() {
-//        let home = UINavigationController(rootViewController: HomeViewController())
-        let home = UINavigationController(rootViewController: PinDetailViewController())
+        let home = UINavigationController(rootViewController: HomeViewController())
+        
         home.tabBarItem = UITabBarItem(title: "Home",
                                        image: UIImage(systemName: "house"),
                                        selectedImage: UIImage(systemName: "house.fill"))
